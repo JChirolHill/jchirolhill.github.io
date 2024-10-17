552b0ce9bd19ba047c0c9535d417f84193bfbac3
JChirolHill <jchirolhill@zoho.com>
2024-10-16 20:54:52 -0700

    Added tags

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,16 +4,39 @@ $(document).ready(function () {
 
     const projectTemplate = Handlebars.compile($("#template_project").html());
 
-    fetch("data.json")
-        .then((response) => response.json())
-        .then((projects) => {
-            // Dynamically generate all projects
-            // Render visible those already scrolled to
-            const $projectListing = $("#projectListing");
-            $.each(projects, function (index, project) {
-                // if (project.tags.includes("Language Learning")) {
-                $projectListing.append(projectTemplate(project));
-                // }
+    let selectedTag = "featured";
+    $("#tags div").click(function (event) {
+        console.log(event.target.id);
+        if (event.target.id !== selectedTag) {
+            // Remove existing tag
+            $(".selectedTag").removeClass("selectedTag");
+
+            // Change to new tag
+            selectedTag = event.target.id;
+            $(this).addClass("selectedTag");
+            fetchProjects();
+        }
+    });
+
+    const fetchProjects = () => {
+        // Clear existing projects
+        const $projectListing = $("#projectListing");
+        $projectListing.empty();
+
+        fetch("data.json")
+            .then((response) => response.json())
+            .then((projects) => {
+                // Dynamically generate all projects
+                // Render visible those already scrolled to
+                $.each(projects, (_, project) => {
+                    if (
+                        selectedTag === "all" ||
+                        project.tags.includes(selectedTag)
+                    ) {
+                        $projectListing.append(projectTemplate(project));
+                    }
+                });
             });
-        });
+    };
+    fetchProjects();
 });
